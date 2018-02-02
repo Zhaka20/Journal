fc97e66f6523b171254494a489ececbc47efa920
Olzhas <badawisker@gmail.com>
2018-02-02 11:31:27 +0600

    some changes

---
+++ AbstractBLL/Services/Common/IFilterOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBLL.Services.Common
+{
+    public abstract class AbstractFilterOptions<TKey>
+    {
+        IList<string> IncludeProperties { get; set; }
+        IList<Dictionary<string,SortOrder>> OrderBy { get; set; }
+        bool FinById { get; set; }
+        TKey Id { get; set; }
+        int Skip { get; set; }
+        int Take { get; set; }
+    }
+
+    public enum SortOrder
+    {
+        Ascending,
+        Descending
+    }
+}
