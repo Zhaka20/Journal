8c366c9e2f3c43c321caf0f51ea3c0d9cf73431c
Olzhas <badawisker@gmail.com>
2018-02-06 09:05:56 +0600

    change

--- Journal/Controllers/CommentsController.cs
+++ Journal/Controllers/CommentsController.cs
@@ -1,142 +1,142 @@
-﻿using System;
-using System.Data.Entity;
-using System.Threading.Tasks;
-using System.Net;
-using System.Web.Mvc;
-using Journal.DAL.Context;
-using Microsoft.AspNet.Identity;
+﻿//using System;
+//using System.Data.Entity;
+//using System.Threading.Tasks;
+//using System.Net;
+//using System.Web.Mvc;
+//using Journal.DAL.Context;
+//using Microsoft.AspNet.Identity;
 
-namespace Journal.Controllers
-{
-    public class CommentsController : Controller
-    {
-    //    private ApplicationDbContext db;
+//namespace Journal.Controllers
+//{
+//    public class CommentsController : Controller
+//    {
+//        private ApplicationDbContext db;
 
-    //    public CommentsController(ApplicationDbContext db)
-    //    {
-    //        this.db = db;
-    //    }
-    //    // GET: Comments
-    //    public async Task<ActionResult> Index()
-    //    {
-    //        var comments = db.Comments.Include(c => c.Assignment).Include(c => c.Author);
-    //        return View(await comments.ToListAsync());
-    //    }
+//        public CommentsController(ApplicationDbContext db)
+//        {
+//            this.db = db;
+//        }
+//        // GET: Comments
+//        public async Task<ActionResult> Index()
+//        {
+//            var comments = db.Comments.Include(c => c.Assignment).Include(c => c.Author);
+//            return View(await comments.ToListAsync());
+//        }
 
-    //    // GET: Comments/Details/5
-    //    public async Task<ActionResult> Details(int? id)
-    //    {
-    //        if (id == null)
-    //        {
-    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    //        }
-    //        Comment comment = await db.Comments.FindAsync(id);
-    //        if (comment == null)
-    //        {
-    //            return HttpNotFound();
-    //        }
-    //        return View(comment);
-    //    }
+//        // GET: Comments/Details/5
+//        public async Task<ActionResult> Details(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            Comment comment = await db.Comments.FindAsync(id);
+//            if (comment == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(comment);
+//        }
 
-    //    // GET: Comments/Create
-    //    public ActionResult Create()
-    //    {
-    //        ViewBag.AssignmentId = new SelectList(db.Assignments, "AssignmentId", "Title");
-    //        ViewBag.AuthorId = new SelectList(db.Users, "Id", "FirstName");
-    //        return View();
-    //    }
+//        // GET: Comments/Create
+//        public ActionResult Create()
+//        {
+//            ViewBag.AssignmentId = new SelectList(db.Assignments, "AssignmentId", "Title");
+//            ViewBag.AuthorId = new SelectList(db.Users, "Id", "FirstName");
+//            return View();
+//        }
 
-    //    // POST: Comments/Create
-    //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-    //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-    //    [Authorize(Roles = "Mentor, Student")]
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public async Task<ActionResult> Create(Comment comment, int id)
-    //    {
-    //        //check if user is owner or mentor is users current mentor!
-    //        if (ModelState.IsValid)
-    //        {
-    //            comment.Created = DateTime.Now;
-    //            comment.AssignmentId = id;
-    //            comment.AuthorId = User.Identity.GetUserId();
-    //            db.Comments.Add(comment);
-    //            await db.SaveChangesAsync();
-    //            return RedirectToAction("Details", "Assignments" , new { id = id });
-    //        }
-    //        return View(comment);
-    //    }
+//        // POST: Comments/Create
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [Authorize(Roles = "Mentor, Student")]
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public async Task<ActionResult> Create(Comment comment, int id)
+//        {
+//            //check if user is owner or mentor is users current mentor!
+//            if (ModelState.IsValid)
+//            {
+//                comment.Created = DateTime.Now;
+//                comment.AssignmentId = id;
+//                comment.AuthorId = User.Identity.GetUserId();
+//                db.Comments.Add(comment);
+//                await db.SaveChangesAsync();
+//                return RedirectToAction("Details", "Assignments", new { id = id });
+//            }
+//            return View(comment);
+//        }
 
-    //    // GET: Comments/Edit/5
-    //    public async Task<ActionResult> Edit(int? id)
-    //    {
-    //        if (id == null)
-    //        {
-    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    //        }
-    //        Comment comment = await db.Comments.FindAsync(id);
-    //        if (comment == null)
-    //        {
-    //            return HttpNotFound();
-    //        }
-    //        return View(comment);
-    //    }
+//        // GET: Comments/Edit/5
+//        public async Task<ActionResult> Edit(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            Comment comment = await db.Comments.FindAsync(id);
+//            if (comment == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(comment);
+//        }
 
-    //    // POST: Comments/Edit/5
-    //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-    //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public async Task<ActionResult> Edit([Bind(Include = "Id,Text,AssignmentId")] Comment comment)
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            comment.Edited = DateTime.Now;
-    //            db.Comments.Attach(comment);
-    //            db.Entry(comment).Property(c => c.Edited).IsModified = true;
-    //            db.Entry(comment).Property(c => c.Text).IsModified = true;
+//        // POST: Comments/Edit/5
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public async Task<ActionResult> Edit([Bind(Include = "Id,Text,AssignmentId")] Comment comment)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                comment.Edited = DateTime.Now;
+//                db.Comments.Attach(comment);
+//                db.Entry(comment).Property(c => c.Edited).IsModified = true;
+//                db.Entry(comment).Property(c => c.Text).IsModified = true;
 
-    //            await db.SaveChangesAsync();
-    //            return RedirectToAction("Details", "Assignments", new { id = comment.AssignmentId });
-    //        }
-    //        ViewBag.AssignmentId = new SelectList(db.Assignments, "AssignmentId", "Title", comment.AssignmentId);
-    //        ViewBag.AuthorId = new SelectList(db.Users, "Id", "FirstName", comment.AuthorId);
-    //        return View(comment);
-    //    }
+//                await db.SaveChangesAsync();
+//                return RedirectToAction("Details", "Assignments", new { id = comment.AssignmentId });
+//            }
+//            ViewBag.AssignmentId = new SelectList(db.Assignments, "AssignmentId", "Title", comment.AssignmentId);
+//            ViewBag.AuthorId = new SelectList(db.Users, "Id", "FirstName", comment.AuthorId);
+//            return View(comment);
+//        }
 
-    //    // GET: Comments/Delete/5
-    //    public async Task<ActionResult> Delete(int? id)
-    //    {
-    //        if (id == null)
-    //        {
-    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    //        }
-    //        Comment comment = await db.Comments.FindAsync(id);
-    //        if (comment == null)
-    //        {
-    //            return HttpNotFound();
-    //        }
-    //        return View(comment);
-    //    }
+//        // GET: Comments/Delete/5
+//        public async Task<ActionResult> Delete(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            Comment comment = await db.Comments.FindAsync(id);
+//            if (comment == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(comment);
+//        }
 
-    //    // POST: Comments/Delete/5
-    //    [HttpPost, ActionName("Delete")]
-    //    [ValidateAntiForgeryToken]
-    //    public async Task<ActionResult> DeleteConfirmed(int id)
-    //    {
-    //        Comment comment = await db.Comments.FindAsync(id);
-    //        db.Comments.Remove(comment);
-    //        await db.SaveChangesAsync();
-    //        return RedirectToAction("Details", "Assignments", new { id = comment.AssignmentId });
-    //    }
+//        // POST: Comments/Delete/5
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public async Task<ActionResult> DeleteConfirmed(int id)
+//        {
+//            Comment comment = await db.Comments.FindAsync(id);
+//            db.Comments.Remove(comment);
+//            await db.SaveChangesAsync();
+//            return RedirectToAction("Details", "Assignments", new { id = comment.AssignmentId });
+//        }
 
-    //    protected override void Dispose(bool disposing)
-    //    {
-    //        if (disposing)
-    //        {
-    //            db.Dispose();
-    //        }
-    //        base.Dispose(disposing);
-    //    }
-    //}
-}
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                db.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+//    }
+//}
---
+++ Journal/nodes.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace Journal
+{
+    public class nodes
+    {
+        static void Main(string[] args)
+        {
+            var man = new Person
+            {
+                Age = 33,
+                FirstName = "Nick",
+                Pet = new Animal { Name = "Dog", Height = 13 }
+            };
+
+            var man2 = new Person
+            {
+                Age = 22,
+                FirstName = "Bravo",
+                Pet = new Animal { Name = "Cat", Height = 13 }
+            };
+
+            Expression<Func<Person, object>> expr1 = arg => arg.Pet;
+            Expression<Func<Person, object>> expr2 = arg => arg.Pet.Name;
+            var compareResult = ExpressionComparer.IsSameMember(expr1, expr2);
+            Console.WriteLine(compareResult);
+            Console.ReadLine();
+        }
+    }
+
+    public static class ExpressionComparer
+    {
+        public static MemberExpression GetMemberExpression<T>(Expression<Func<T, object>> expr)
+        {
+            var member = expr.Body as MemberExpression;
+            var unary = expr.Body as UnaryExpression;
+            return member ?? (unary != null ? unary.Operand as MemberExpression : null);
+        }
+
+        public static bool IsSameMember<T>(this Expression<Func<T, object>> expr1, Expression<Func<T, object>> expr2)
+        {
+            var result1 = GetMemberExpression(expr1);
+            var result2 = GetMemberExpression(expr2);
+
+            if (result1 == null || result2 == null)
+                return false;
+
+            return result1.Member.Name == result2.Member.Name;
+        }
+
+        public static bool IsSameProperty<TSourceA, TSourceB, TPropertyA, TPropertyB>(
+                                                                                       Expression<Func<TSourceA, TPropertyA>> expA,
+                                                                                       Expression<Func<TSourceB, TPropertyB>> expB)
+        {
+            MemberExpression memExpA = expA.Body as MemberExpression;
+            MemberExpression memExpB = expB.Body as MemberExpression;
+
+            if (memExpA == null || memExpB == null)
+                return false;
+
+            PropertyInfo propA = memExpA.Member as PropertyInfo;
+            PropertyInfo propB = memExpB.Member as PropertyInfo;
+
+            if (propA == null || propB == null)
+                return false;
+
+            return propA.Equals(propB);
+        }
+    }
+
+
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public Animal Pet { get; set; }
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+}
+}
